7b0b4c53eb98cb385876ccc500f241e6d6256c32
zyj <zhongyinjiebit@sina.com>
2020-10-25 23:23:57 +0800

    GIF_separate complete

--- GIF_separate/GIF_separate.py
+++ GIF_separate/GIF_separate.py
@@ -1,20 +1,44 @@
 from PIL import Image
-import os
+import os, sys
 
-gif_fullname = '1.gif'# 将准备好的gif 打开
-im = Image.open(gif_fullname)
-pngDir = gif_fullname[:-4]        # 获取 .gif 前面的字符，也就是名字
+gif_path = input("输入要分解的GIF图片文件名：");
+gif_dirname, gif_basename = os.path.split(gif_path)#分解路径
+print('gif_dirname = ', gif_dirname, 'gif_basename = ', gif_basename)
+root, extension = os.path.splitext(gif_basename)#分解扩展名
 
-if not os.path.exists(pngDir):
+#检查是不是GIF文件
+print("root = ", root, "extension = ", extension)
+if gif_basename == '':
+	print(gif_path, "不是GIF文件,是一个目录")
+	sys.exit()
+if extension != '.gif' and extension !='.GIF':
+	print(gif_path, "不是GIF文件")
+	sys.exit()
+
+try:
+	im = Image.open(gif_path)# 将准备好的gif打开
+except FileNotFoundError:
+	print(gif_path, '文件不存在')
+	sys.exit()
+except LookupError:
+	print('指定了未知的编码!')
+	sys.exit()
+except UnicodeDecodeError:
+	print('读取文件时解码错误!')
+	sys.exit()
+
+png_path = os.path.join(os.getcwd(), root)
+print("pngdir=", png_path)
+if not os.path.exists(png_path):
 	'''如果没有重名的文件夹，就生成这个文件夹来存放图片'''
-	os.mkdir(pngDir)         
+	os.mkdir(png_path)         
 
 try:      
 	'''while True 的作用就是不停的遍历gif，取得每一个图片，如果图片访问结束 会报错，所以 try一下'''
 	while True:
 		current = im.tell() # 获取img对象的 帧图片
 		filename = str(current) + '.png'
-		fullname = os.path.join(pngDir,filename)#win or linux
+		fullname = os.path.join(png_path,filename)#win or linux
 		print(fullname)
 		im.save(fullname)# 保存
 		im.seek(current + 1)     # seek的作用就相当于 装饰器的 next，代表下一个
